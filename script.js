b81d7aa6e871b4bfd28cc5031c5608ea0033942b
AlexanderGrayson-RPW <76930519+AlexanderGrayson-RPW@users.noreply.github.com>
2022-11-01 12:11:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function Login() {
     username = document.getElementById("username").value;
     password = document.getElementById("password").value;
 
-    if(username == "alexgray" && password == "Alexander1107") {
+    if(username == "alexrpw" && password == "Alexander0407") {
         alert("Login Successfully!");
         window.location.href="idgenerator.html";
         return false;
