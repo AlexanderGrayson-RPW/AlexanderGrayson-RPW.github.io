6b0eda1c6d5735d8c08817fa7e34f0be65158076
AlexanderGrayson-RPW <76930519+AlexanderGrayson-RPW@users.noreply.github.com>
2022-11-01 12:37:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function Login() {
     username = document.getElementById("username").value;
     password = document.getElementById("password").value;
 
-    if(username == "alexrpw" && password == "Alexander0407") {
+    if(username == "Alex" && password == "Alexander1107") {
         alert("Login Successfully!");
         window.location.href="idgenerator.html";
         return false;
     }else{
-        alert("TANGINA KA AH TRY TRY KA PA");
+        alert("KA AH TRY TRY KA PA");
         return false;
     }
 }
