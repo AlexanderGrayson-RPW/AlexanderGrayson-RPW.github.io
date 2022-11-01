ef9dea36fa8a500c36ec7a2b626ea3a96db85c52
AlexanderGrayson-RPW <76930519+AlexanderGrayson-RPW@users.noreply.github.com>
2022-11-01 11:14:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,47 @@ function Login() {
     username = document.getElementById("username").value;
     password = document.getElementById("password").value;
 
-    if(username == "AlexRPW" && password == "Alexander1107") {
-        alert("LOGIN SUCCESSFULLY!");
-        window.location.href="idgenerator.html";
+    if(username == "alexgray" && password == "Alexander1107") {
+        alert("Login Successfully!");
+        window.location.href="index.html";
         return false;
-    } else {
-        alert("TANGINA KA! KUHA KO NA IP ADDDRESS MO. TRY TRY KA PA HA");
+    }else{
+        alert("TANGINA KA AH TRY TRY KA PA");
         return false;
     }
 }
+
+const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    //   js code to show/hide password and change icon
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
+
+    // js code to appear signup and login form
+    signUp.addEventListener("click", ( )=>{
+        container.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container.classList.remove("active");
+    });
