705a9af66ca5bc961f2771110be17bb7e39ce5a4
AlexanderGrayson-RPW <76930519+AlexanderGrayson-RPW@users.noreply.github.com>
2022-11-01 11:31:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,45 +4,10 @@ function Login() {
 
     if(username == "alexgray" && password == "Alexander1107") {
         alert("Login Successfully!");
-        window.location.href="idgenerator.html";
+        window.location.href=".html";
         return false;
     }else{
         alert("TANGINA KA AH TRY TRY KA PA");
         return false;
     }
 }
-
-const container = document.querySelector(".container"),
-      pwShowHide = document.querySelectorAll(".showHidePw"),
-      pwFields = document.querySelectorAll(".password"),
-      signUp = document.querySelector(".signup-link"),
-      login = document.querySelector(".login-link");
-
-    //   js code to show/hide password and change icon
-    pwShowHide.forEach(eyeIcon =>{
-        eyeIcon.addEventListener("click", ()=>{
-            pwFields.forEach(pwField =>{
-                if(pwField.type ==="password"){
-                    pwField.type = "text";
-
-                    pwShowHide.forEach(icon =>{
-                        icon.classList.replace("uil-eye-slash", "uil-eye");
-                    })
-                }else{
-                    pwField.type = "password";
-
-                    pwShowHide.forEach(icon =>{
-                        icon.classList.replace("uil-eye", "uil-eye-slash");
-                    })
-                }
-            }) 
-        })
-    })
-
-    // js code to appear signup and login form
-    signUp.addEventListener("click", ( )=>{
-        container.classList.add("active");
-    });
-    login.addEventListener("click", ( )=>{
-        container.classList.remove("active");
-    });
