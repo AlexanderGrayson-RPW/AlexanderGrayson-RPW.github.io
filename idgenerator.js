ca0ece3d214fd20d3b99c76a24fe9d2c11351c73
AlexanderGrayson-RPW <76930519+AlexanderGrayson-RPW@users.noreply.github.com>
2023-01-19 03:57:53 +0800

    Update idgenerator.js

--- idgenerator.js
+++ idgenerator.js
@@ -304,3 +304,59 @@ buttons.forEach(btn => {
     btn.addEventListener("click", () => createToast(btn.id));
 
 });*/
+
+function notify(type,message){
+
+  (()=>{
+
+    let n = document.createElement("div");
+
+    let id = Math.random().toString(36).substr(2,10);
+
+    n.setAttribute("id",id);
+
+    n.classList.add("notification",type);
+
+    n.innerText = message;
+
+    document.getElementById("notification-area").appendChild(n);
+
+    setTimeout(()=>{
+
+      var notifications = document.getElementById("notification-area").getElementsByClassName("notification");
+
+      for(let i=0;i<notifications.length;i++){
+
+        if(notifications[i].getAttribute("id") == id){
+
+          notifications[i].remove();
+
+          break;
+
+        }
+
+      }
+
+    },5000);
+
+  })();
+
+}
+
+function notifySuccess(){
+
+  notify("success","The image was successfully downloaded.");
+
+}
+
+function notifyError(){
+
+  notify("error","Fatal Error: This session cannot be logged out.");
+
+}
+
+function notifyInfo(){
+
+  notify("info","The canvas was successfully cleared.");
+
+}
