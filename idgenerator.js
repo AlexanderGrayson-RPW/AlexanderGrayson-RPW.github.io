03b09ee7f27d977732c49605ec991adfce974a73
AlexanderGrayson-RPW <76930519+AlexanderGrayson-RPW@users.noreply.github.com>
2023-02-02 12:30:37 +0800

    Update idgenerator.js

--- idgenerator.js
+++ idgenerator.js
@@ -12,7 +12,6 @@ const downloadBtn = document.getElementById('download-btn')
 const idImage = document.getElementById("imgDisplayed");
 var button = document.getElementById('logoutButton');
 
-
 /*button.addEventListener("click", () => {
     alert("Do you want to logout?");
     window.location.href = "index.html";
@@ -26,7 +25,6 @@ image.onload = function () {
 
 const width = image.width;
 const height = image.height;
-
 tinCanvas.width = width;
 tinCanvas.height = height;
 
@@ -113,13 +111,8 @@ clearCanvas.addEventListener("click", () => {
 });
 
 var logoutBtn = document.getElementById("logout-btn");
-
 logoutBtn.addEventListener("click", function() {
-
-
-
     location.href = "index.html";
-
 });
 
 const download = document.getElementById("down");
@@ -212,98 +205,6 @@ function showMap() {
         document.getElementById("ofwID").style.display = 'none'
     }
 }
-
-/*const notifications = document.querySelector(".notifications"),
-
-buttons = document.querySelectorAll(".buttons .btn");
-
-// Object containing details for different types of toasts
-
-const toastDetails = {
-
-    timer: 10000,
-
-    down: {
-
-        icon: 'fa-circle-check',
-
-        text: 'Download: The image was successfully downloaded.',
-
-    },
-
-    logout-button: {
-
-        icon: 'fa-circle-xmark',
-
-        text: 'Fatal Error: The session cannot be logged out.',
-
-    },
-
-    note: {
-
-        icon: 'fa-triangle-exclamation',
-
-        text: 'Note: Upload the 1x1 photo ID before filling up.',
-
-    },
-
-    clear: {
-
-        icon: 'fa-circle-info',
-
-        text: 'Clear: The canvas was successfully cleared.',
-
-    }
-
-}
-
-const removeToast = (toast) => {
-
-    toast.classList.add("hide");
-
-    if(toast.timeoutId) clearTimeout(toast.timeoutId); // Clearing the timeout for the toast
-
-    setTimeout(() => toast.remove(), 10000); // Removing the toast after 500ms
-
-}
-
-const createToast = (id) => {
-
-    // Getting the icon and text for the toast based on the id passed
-
-    const { icon, text } = toastDetails[id];
-
-    const toast = document.createElement("li"); // Creating a new 'li' element for the toast
-
-    toast.className = `toast ${id}`; // Setting the classes for the toast
-
-    // Setting the inner HTML for the toast
-
-    toast.innerHTML = `<div class="column">
-
-                         <i class="fa-solid ${icon}"></i>
-
-                         <span>${text}</span>
-
-                      </div>
-
-                      <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
-
-    notifications.appendChild(toast); // Append the toast to the notification ul
-
-    // Setting a timeout to remove the toast after the specified duration
-
-    toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
-
-}
-
-// Adding a click event listener to each button to create a toast when clicked
-
-buttons.forEach(btn => {
-
-    btn.addEventListener("click", () => createToast(btn.id));
-
-});*/
 
 function notify(type,message){
 
