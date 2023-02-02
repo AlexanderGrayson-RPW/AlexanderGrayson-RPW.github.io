08d6973934f90b9fbc4b961b7aca46965bf9fd1e
AlexanderGrayson-RPW <76930519+AlexanderGrayson-RPW@users.noreply.github.com>
2023-02-02 12:56:20 +0800

    Add files via upload

---
+++ showID.js
@@ -0,0 +1,81 @@
+function showMap() {
+    country = document.getElementById("country").value;
+    if (country == "TIN") {
+        document.getElementById("tinID").style.display = 'block'
+    } else {
+        document.getElementById("tinID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "NATIONAL") {
+        document.getElementById("natID").style.display = 'block'
+    }
+    else {
+        document.getElementById("natID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "VOTER") {
+        document.getElementById("votersID").style.display = 'block'
+    }
+    else {
+        document.getElementById("votersID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "PASSPORT") {
+        document.getElementById("passportID").style.display = 'block'
+    }
+    else {
+        document.getElementById("passportID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "DRIVER") {
+        document.getElementById("driversID").style.display = 'block'
+    }
+    else {
+        document.getElementById("driversID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "POSTAL") {
+        document.getElementById("postalID").style.display = 'block'
+    }
+    else {
+        document.getElementById("postalID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "PRC") {
+        document.getElementById("prcID").style.display = 'block'
+    }
+    else {
+        document.getElementById("prcID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "PHILHEALTH") {
+        document.getElementById("philhealthID").style.display = 'block'
+    }
+    else {
+        document.getElementById("philhealthID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "SSS") {
+        document.getElementById("sssID").style.display = 'block'
+    }
+    else {
+        document.getElementById("sssID").style.display = 'none'
+    }
+
+    country = document.getElementById("country").value;
+    if (country == "OFW") {
+        document.getElementById("ofwID").style.display = 'block'
+    }
+    else {
+        document.getElementById("ofwID").style.display = 'none'
+    }
+
+}
