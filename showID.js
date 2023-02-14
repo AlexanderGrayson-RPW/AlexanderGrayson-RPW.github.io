ca26d7abd4f26117459f15f8e3ef739031474b6e
AlexanderGrayson-RPW <76930519+AlexanderGrayson-RPW@users.noreply.github.com>
2023-02-14 19:42:58 +0800

    Update showID.js

--- showID.js
+++ showID.js
@@ -1,81 +1,48 @@
-function showMap() {
-    country = document.getElementById("country").value;
-    if (country == "TIN") {
+function showID() {
+    validID = document.getElementById("validID").value;
+    if (validID == "TIN") {
         document.getElementById("tinID").style.display = 'block'
     } else {
         document.getElementById("tinID").style.display = 'none'
     }
 
-    country = document.getElementById("country").value;
-    if (country == "NATIONAL") {
+    validID = document.getElementById("validID").value;
+    if (validID == "NATIONAL") {
         document.getElementById("natID").style.display = 'block'
     }
     else {
         document.getElementById("natID").style.display = 'none'
     }
 
-    country = document.getElementById("country").value;
-    if (country == "VOTER") {
+    validID = document.getElementById("validID").value;
+    if (validID == "VOTER") {
         document.getElementById("votersID").style.display = 'block'
     }
     else {
         document.getElementById("votersID").style.display = 'none'
     }
 
-    country = document.getElementById("country").value;
-    if (country == "PASSPORT") {
-        document.getElementById("passportID").style.display = 'block'
-    }
-    else {
-        document.getElementById("passportID").style.display = 'none'
-    }
-
-    country = document.getElementById("country").value;
-    if (country == "DRIVER") {
+    validID = document.getElementById("validID").value;
+    if (validID == "DRIVER") {
         document.getElementById("driversID").style.display = 'block'
     }
     else {
         document.getElementById("driversID").style.display = 'none'
     }
 
-    country = document.getElementById("country").value;
-    if (country == "POSTAL") {
+    validID = document.getElementById("validID").value;
+    if (validID == "POSTAL") {
         document.getElementById("postalID").style.display = 'block'
     }
     else {
         document.getElementById("postalID").style.display = 'none'
     }
 
-    country = document.getElementById("country").value;
-    if (country == "PRC") {
+    validID = document.getElementById("validID").value;
+    if (validID == "PRC") {
         document.getElementById("prcID").style.display = 'block'
     }
     else {
         document.getElementById("prcID").style.display = 'none'
     }
-
-    country = document.getElementById("country").value;
-    if (country == "PHILHEALTH") {
-        document.getElementById("philhealthID").style.display = 'block'
-    }
-    else {
-        document.getElementById("philhealthID").style.display = 'none'
-    }
-
-    country = document.getElementById("country").value;
-    if (country == "SSS") {
-        document.getElementById("sssID").style.display = 'block'
-    }
-    else {
-        document.getElementById("sssID").style.display = 'none'
-    }
-
-    country = document.getElementById("country").value;
-    if (country == "OFW") {
-        document.getElementById("ofwID").style.display = 'block'
-    }
-    else {
-        document.getElementById("ofwID").style.display = 'none'
-    }
-
 }
